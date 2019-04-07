17867a719ed8a4c04220f7cb7f984e3429d2124a
Catherine <catkcm.16@gmail.com>
2019-04-07 20:29:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!";
   } 
-  else (string.toUpperCase() === string) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   }
 }
